ce2b6987cb4a76ff4dd9e93a32aa0170c379e38c
hoon <hundo1217@gmail.com>
2025-04-21 13:16:06 +0900

    컴포넌트 생성 실습

---
+++ src/component.tsx
@@ -0,0 +1,12 @@
+const Component = () => {
+  return (
+    <div>
+      <h1>Hello, Vite + React!</h1>
+      <p>리액트는 UI를 만드는 라이브러리입니다.</p>
+    </div>
+  );
+}
+
+export default Component; 
+// 위 코드는 리액트 컴포넌트를 정의하는 코드입니다.
+// 리액트 컴포넌트는 UI를 구성하는 기본 단위로, JSX 문법을 사용하여 HTML과 유사한 구조로 작성됩니다.
